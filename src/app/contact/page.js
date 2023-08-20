ecda91bf78d5aedaa56d43b6a4034504157af733
lucasBruma <lucasbrumatti99@gmail.com>
2023-08-20 16:13:53 -0300

    Agrego hero en contacto

---
+++ src/app/contact/page.js
@@ -0,0 +1,12 @@
+import { Hero } from "@/components/Hero";
+
+export default function Page() {
+  return (
+    <main>
+      <Hero title="Contact" />
+      <div>
+        <h1>Contacto</h1>
+      </div>
+    </main>
+  );
+}
