25d64590486b34ba6f1b1330b66d315ddef0aaaf
lucasBruma <lucasbrumatti99@gmail.com>
2023-09-13 09:43:53 -0300

    Utilizo componente image de next en fondos para reducir bundle size

--- src/components/HomeHero/index.jsx
+++ src/components/HomeHero/index.jsx
@@ -2,21 +2,14 @@ import { Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function HomeHero() {
-  const backgroundImageUrl = "url(homeKitchen.jpg)";
-  const backgroundStyle = {
-    backgroundImage: `${backgroundImageUrl}`,
-    backgroundSize: "cover",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  };
-
   return (
     <main>
-      <div
-        className="relative flex h-[980px] flex-col items-start justify-center gap-8 px-6 text-white md:px-32 xl:px-56"
-        style={backgroundStyle}
-      >
-        <div className="absolute inset-0 h-full bg-black opacity-60"></div>
-        <div className="relative z-20 flex w-full flex-col gap-4">
+      <div className="relative flex h-[980px] flex-col items-start justify-center gap-8 px-6 text-white md:px-32 xl:px-56">
+        <div className="absolute inset-0 z-20 h-full">
+          <div className="absolute inset-0 z-30 h-full bg-black opacity-60" />
+          <Image src="/homeKitchen.jpg" width={1000} height={1000} alt="Kitchen" className="h-full w-full" />
+        </div>
+        <div className="relative z-30 flex w-full flex-col gap-4">
           <Typography variant="h2" sx={{ fontFamily: "inherit" }} className="!text-[5rem] !font-bold !leading-[5rem]">
             Explore can make <span className="block italic text-secondary-light">your dream kitchen</span>
           </Typography>
--- src/components/SectionTransparent/index.jsx
+++ src/components/SectionTransparent/index.jsx
@@ -1,25 +1,19 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import Image from "next/image";
 
 export const SectionTransparent = ({ title }) => {
-  const backgroundImageUrl = "url(kitchen.png)";
-  const backgroundStyle = {
-    backgroundImage: `${backgroundImageUrl}`,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-  };
-
   return (
-    <div
-      className="relative flex w-full select-none flex-col items-center justify-center px-32 py-24 text-center text-white md:px-56"
-      style={backgroundStyle}
-    >
-      <div className="absolute inset-0 bg-primary-main opacity-80"></div>
+    <div className="relative flex w-full select-none flex-col items-center justify-center px-32 py-24 text-center text-white md:px-56">
+      <div className="absolute inset-0 z-20 h-full">
+        <div className="absolute inset-0 z-30 h-full bg-primary-main opacity-80" />
+        <Image src="/kitchen.png" width={1000} height={500} alt="Kitchen" className="h-full w-full object-cover" />
+      </div>
       <Typography
         sx={{ fontFamily: "inherit" }}
         component="h1"
         variant="h6"
-        className="relative z-10 !font-normal !italic"
+        className="relative z-40 !font-normal !italic"
       >
         {title}
       </Typography>
