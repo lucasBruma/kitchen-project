b3fd1c4a741d6dda28556a432c10984c1f30fcdf
lucasBruma <lucasbrumatti99@gmail.com>
2023-10-13 16:17:50 -0300

    agrego botones al modal de la galeria

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,7 +2,7 @@
 import styled from "@emotion/styled";
 import { useState } from "react";
 import { Button } from "@mui/material";
-import { IoMdClose } from "react-icons/io";
+import { IoMdClose, IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 const ImageShadow = styled.div`
   background: rgb(0, 0, 0);
@@ -11,16 +11,28 @@ const ImageShadow = styled.div`
 
 export function Gallery({ images = [] }) {
   const [isModalOpen, setModalOpen] = useState(false);
-  const [currentImage, setCurrentImage] = useState(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(null);
+
+  const goToNextImage = () => {
+    if (currentImageIndex < images.length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  };
+
+  const goToPreviousImage = () => {
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  };
 
   return (
     <div className="grid w-full grid-cols-1 grid-rows-1 gap-4 px-6 py-12 md:grid-cols-2 md:px-32 lg:grid-cols-3 lg:gap-8 xl:px-56">
-      {images.map((image) => (
+      {images.map((image, index) => (
         <div key={image.src} className="h-full w-full">
           <Button
             className="group relative w-full cursor-pointer overflow-hidden rounded-md border-2 border-white p-0 duration-300 ease-in hover:shadow-lg hover:shadow-gray-400"
             onClick={() => {
-              setCurrentImage(image);
+              setCurrentImageIndex(index);
               setModalOpen(true);
             }}
           >
@@ -34,12 +46,24 @@ export function Gallery({ images = [] }) {
           {isModalOpen && (
             <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-10">
               <div className="relative rounded-lg bg-white p-4 shadow-lg">
-                <img src={currentImage.src} alt="Current View" />
+                <img src={images[currentImageIndex]?.src} alt="Current View" className="max-h-[700px]" />
                 <button
                   className="absolute right-[-20px] top-[-20px] rounded-[50%] bg-red-500 p-2 text-white"
                   onClick={() => setModalOpen(false)}
                 >
                   <IoMdClose size={50} />
+                </button>
+                <button
+                  className="absolute left-[-35px] top-[50%] -translate-y-1/2 transform rounded-[50%] bg-black p-2 text-white"
+                  onClick={goToPreviousImage}
+                >
+                  <IoIosArrowBack size={50} />
+                </button>
+                <button
+                  className="absolute right-[-35px] top-[50%] -translate-y-1/2 transform rounded-[50%] bg-black p-2 text-white"
+                  onClick={goToNextImage}
+                >
+                  <IoIosArrowForward size={50} />
                 </button>
               </div>
             </div>
