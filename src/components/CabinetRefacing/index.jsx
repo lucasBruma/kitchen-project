d1908c79ca9bcf05c8080b6ea73ea6e1bd795d2e
lucasBruma <lucasbrumatti99@gmail.com>
2023-10-22 17:40:55 -0300

    agrego faqs seccion

--- src/components/CabinetRefacing/index.jsx
+++ src/components/CabinetRefacing/index.jsx
@@ -2,8 +2,8 @@
 import { Hero } from "@/components/Hero";
 import React from "react";
 import styled from "@emotion/styled";
-import Link from "next/link";
 import { Intro } from "./Intro";
+import { Faqs } from "./Faqs";
 
 const Container = styled.div`
   background: linear-gradient(90deg, rgba(56, 55, 65, 0.3) -80%, rgba(250, 250, 250, 0) 100%);
@@ -35,6 +35,7 @@ export const CabinetRefacing = () => {
             <div className="p-5 pb-12 text-start md:pb-20">
               <h2 className="text-4xl font-bold">Who Should You Trust to Perform Cabinet Refacing?</h2>
             </div>
+            <Faqs />
           </div>
         </div>
       </Container>
